64c40e3da3084792853d2601e3c622e7971e39df
carl <carlos.acitores@colba.es>
2026-02-26 16:45:06 +0100

    fixes

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -12,7 +12,7 @@ export async function POST(req: NextRequest) {
     fetch(body.destinationUrl, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ payload: body.payload }),
+      body: JSON.stringify(body) ,
     });
   }
 
